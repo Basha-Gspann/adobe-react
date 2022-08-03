3727013085419088d599e93c3a3e257c74b7e26b
Rabbu-G <105190642+Rabbu-G@users.noreply.github.com>
2022-08-03 13:59:56 +0530

    .

--- src/component/Header/Navmob.jsx
+++ src/component/Header/Navmob.jsx
@@ -48,13 +48,13 @@ const Navmob = ({ setCategory, selectedCategory, ...props }) => {
             </NavLink>
             {/* <NavLink to="/cart" className="btn btn-outline-dark ms-2"> <i className="fa fa-shopping-cart me-1"></i>Cart(0)</NavLink> */}
             <NavLink to="/cart" className="cart">
-              {" "}
+              
               {props.countCartItems ? (
                 <p className="btn">{props.countCartItems}</p>
               ) : (
                 ""
               )}
-            </NavLink>{" "}
+            </NavLink>
           </div>
 
           <div className="menu-items">
